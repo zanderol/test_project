87a5e7638ae88891c6858213aa43c08c01ed35b7
zanderol <olexandruk@gmail.com>
2018-11-16 14:05:31 +0200

    Finish routing of test_project

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'test_project/home'
+  root 'test_project#home'
   get 'test_project/help'
   get 'test_project/about'
-  get 'attachments/index'
-  get 'attachments/new'
-  get 'attachments/create'
-  get 'attachments/destroy'
+  # get 'attachments/index'
+  # get 'attachments/new'
+  # get 'attachments/create'
+  # get 'attachments/destroy'
+  resources :test_projects
   resources :comments
   resources :users
   resources :tasks
   resources :projects
   resources :attachments, only: [:index, :new, :create, :destroy]
-  root 'projects#index'
+  # root 'projects#index'
 end
